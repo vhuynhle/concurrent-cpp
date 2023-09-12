07d345defc1290ac0b5c028550bb9b86c39b95af
Van Huynh Le <v.h.le@protonmail.com>
2023-09-12 20:09:45 +0200

    Create Singleton with once_flag and call_once

---
+++ 03-multithreading/SingletonCallOnce.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <mutex>
+
+class Singleton {
+
+public:
+    static Singleton& getInstance()
+    {
+        std::call_once(oflag, [] { instance = new Singleton {}; });
+        return *instance;
+    }
+
+private:
+    static std::once_flag oflag;
+    static Singleton* instance;
+
+    Singleton()
+    {
+        std::cout << "Singleton constructed.\n";
+    }
+};
+
+std::once_flag Singleton::oflag;
+Singleton* Singleton::instance;
+
+int main()
+{
+    auto& instance_1 = Singleton::getInstance();
+    auto& instance_2 = Singleton::getInstance();
+
+    if (&instance_1 != &instance_2) {
+        std::cout << "NOT A SINGLETON!\n";
+    } else {
+        std::cout << "Ok\n";
+    }
+
+    return 0;
+}
